eeb005a44f138daa8013e1890e092cbe19b8efbb
igearproject <igedearya359@gmail.com>
2022-09-17 13:48:42 +0700

    add list address cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 
 connectDB();
 // cors configure
-const whitelist = ['http://localhost:3000','http://127.0.0.1:3000']
+const whitelist = ['http://localhost:3000','http://127.0.0.1:3000','https://dotsdizzy.vercel.app', 'https://dotsdizzy.sipintek.com']
 const corsOptions = {
     origin:whitelist,
     credentials: true,
